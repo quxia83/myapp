6186434645859cb3cc56943d47fe62add5465242
Xia Qu <quxiapisces@gmail.com>
2024-05-26 17:49:50 -0500

    tags with build number and date

--- build.sh
+++ build.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 set -x
-# Variables
-REPO_NAME="quxia/myapp"
-TAG="latest"
 
-# Build the Docker image using the multi-stage Dockerfile
-docker build -t $REPO_NAME:$TAG .
+# Variables
+BUILD_DATE=$(date +%Y%m%d)
+BUILD_NUMBER=%build.number%
+
+REPO_NAME="quxia/myapp"
+
+# Tags
+TAG_DATE_BUILD="$REPO_NAME:$BUILD_DATE-$BUILD_NUMBER"
+TAG_LATEST="$REPO_NAME:latest"
+
+# Build the Docker image
+docker build -t $TAG_DATE_BUILD -t $TAG_LATEST .
 
 # Docker Login (Ensure this is secure in real use, possibly use secrets management)
 # Replace USERNAME and PASSWORD with your Docker Hub credentials
 # For CI/CD use, these should be securely provided by the environment or CI/CD system settings
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
-# Push the image to a registry
-docker push $REPO_NAME:$TAG
+# Push the tagged image with the date and build number
+docker push $TAG_DATE_BUILD
+
+# Push the 'latest' tag
+docker push $TAG_LATEST
